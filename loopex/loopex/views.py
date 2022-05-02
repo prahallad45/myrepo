9ef167d281839f686fbdfbc6b758883634399495
prahallad45 <pgahir1045@gmail.com>
2022-05-02 17:06:33 +0530

    1st commit

---
+++ loopex/loopex/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'home.html')
+
+def color(request):
+    dict1=request.GET
+    name=dict['color']
+    list1=["shirt","pant","shoes","tie"]
+    out= {
+        'name':name,'garments ':list1
+    }
+    return render(request,'color.html',out)
