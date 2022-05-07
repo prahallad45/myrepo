bba8174bbdd9459d87867231cc630507aaa30bd7
prahallad45 <104667710+prahallad45@users.noreply.github.com>
2022-05-07 14:53:06 +0530

    Update allvalue.py

--- list program/allvalue.py
+++ list program/allvalue.py
@@ -1,17 +1,6 @@
-def get_values(d):
-    for v in d.values():
-        if isinstance(v, dict):
-            yield from get_values(v)
-        else:
-            yield v
-
-my_own = {'1':'harsha','name':{'location':{'bangalore':{'marath','white'},'che':{1:{'hy','ra'}}}}}
-
-print(list(get_values(my_own)))
-
-mydict2 = {'1':'harsha','name':{'location':{'bangalore':{'marath','white'},'che':{1:{'hy','ra'}}}}}
+d = {'1':'harsha','name':{'location':{'bangalore':{'marath','white'},'che':{1:{'hy','ra'}}}}}
 nl = []
-nl.append(mydict2['1'])
-nl.append(mydict2['name']['location']['bangalore'])
-nl.append(mydict2['che'][1])
+nl.append(d['1'])
+nl.append(d['name']['location']['bangalore'])
+nl.append(d['name']['location']['che'][1])
 print(nl)
